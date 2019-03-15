ba100c083fc43d1747afbd4ee684651d1687adb0
Ilya Pritchin <pritchin.i@docsvision.com>
2019-03-15 14:25:23 +0300

    Fisrt working test

---
+++ SelStudy/SelStudy/FirstTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace SelStudy
+{
+    [TestClass]
+    public class FirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private const string URL = "http://google.ru";
+        private const string IE_DRIVER_PATH = @"C:\Tools\";
+
+        [TestMethod]
+        public void start()
+        {
+            //driver = new ChromeDriver();
+            var options = new InternetExplorerOptions()
+            {
+                InitialBrowserUrl = URL,
+                IntroduceInstabilityByIgnoringProtectedModeSettings = true
+            };
+            driver = new InternetExplorerDriver(IE_DRIVER_PATH, options);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate();
+            driver.Close(); // closes browser
+            driver.Quit(); // closes IEDriverServer process
+        }
+    }
+}
